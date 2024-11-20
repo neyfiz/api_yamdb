a0238fbb944e65fa410a184923491600d7dbaa7a
neyfiz <ney24fiz@gmail.com>
2024-11-20 14:00:57 +0300

    Update urls.py
    Удалил комментарии

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -25,16 +25,14 @@ v1_router.register(
     r'titles/(?P<title_id>\d+)/reviews', ReviewViewSet, basename='review'
 )
 
-# Вынесение URL с префиксом 'auth' в отдельный список
 auth_urlpatterns = [
     path('signup/', UserViewSet.as_view({'post': 'signup'}), name='signup'),
     path('token/', UserViewSet.as_view({'post': 'token'}), name='token_obtain_pair'),
 ]
 
-# Основной список URL для API v1
 v1_urlpatterns = [
     path('', include(v1_router.urls)),
-    path('auth/', include(auth_urlpatterns)),  # Включаем auth URL в основной список
+    path('auth/', include(auth_urlpatterns)),
 ]
 
 # Общий список URL для всего приложения
