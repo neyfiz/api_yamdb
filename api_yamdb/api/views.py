565122760aa610f1cbdcfc3d0304671a4893b9c8
Neyfiz <ney24fiz@gmail.com>
2024-11-21 17:25:43 +0300

    изменил импорты

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -17,7 +17,6 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
 from rest_framework_simplejwt.tokens import RefreshToken
-
 from reviews.models import Category, Genre, Review, Title, User
 
 from .filters import TitleFilter
